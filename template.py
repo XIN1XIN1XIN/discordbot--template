097b62c4dab3f76cba405e813eb6492b450923d0
XIN <88397521+XIN1XIN1XIN@users.noreply.github.com>
2022-01-04 10:31:08 +0300

    Create template.py

---
+++ template.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+client = commands.Bot(command_prefix = "urprefix")
+
+
+token = "urtoken"
+
+#start the code below here#
+
+
+
+#the middle remove all the # texts when u start the code (ADD AS MUCH CODE AS U WANT IN THE MIDDLE) (the code cant be bot.command it must be client.command instead)
+
+
+
+#end the code#
+
+client.run(token)
